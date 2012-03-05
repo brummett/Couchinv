a6c420638ba2f3c4679eaa9081bfe3b1f5f8dd4b
Anthony Brummett <brummett@gmail.com>
2012-03-05 16:59:57 -0600

    Refactor the Customer activity to use the EditableForm

--- _attachments/manip_customers.js
+++ _attachments/manip_customers.js
@@ -77,77 +77,50 @@ function initial_customer_list() {
 
 
 function customerform(doctoedit) {
-    var formhtml = '<h1>' + (doctoedit ? 'Edit' : 'Add') + ' Customer</h1>';
-    formhtml = formhtml + '<form name="updatecustomer" id="updatecustomer" action="">';
-
+    var fields = [  { type: 'text', label: 'First name', id: 'firstname', validate: 'notblank',
+                      value: (doctoedit ? doctoedit.firstname : '') },
+                    { type: 'text', label: 'Last name', id: 'lastname',
+                      value: (doctoedit ? doctoedit.lastname : '') },
+                    { type: 'textarea', label: 'Address', id: 'address', rows: 4, cols: 40,
+                      value: (doctoedit ? doctoedit.address : '') },
+                    { type: 'text', label: 'Phone number', id: 'phonenumber',
+                      value: (doctoedit ? doctoedit.phonenumber : '') },
+                    { type: 'text', label: 'Email', id: 'email',
+                      value: (doctoedit ? doctoedit.email : '') },
+                    { type: 'textarea', label: 'Notes', id: 'notes', rows: 4, cols: 40,
+                      value: (doctoedit ? doctoedit.notes : '') }
+                ];
     if (doctoedit) {
-	// Editing an existing customer
-	formhtml = formhtml 
-                   + '<input name="docid" id="docid" type="hidden" value="' + doctoedit._id + '"/>';
+        fields.push({ type: 'hidden', id: 'docid', value: doctoedit._id});
     }
-
-    formhtml = formhtml + '<table class="form">'
-	+ '<tr><td>First name</td>'
-	+ '<td><input name="firstname" type="text" id="firstname" value="'
-	    + (doctoedit ? doctoedit.firstname : '')
-	    + '"/></td></tr>'
-
-	+ '<tr><td>Last name</td>'
-	+ '<td><input name="lastname" type="text" id="lastname" value="'
-	    + (doctoedit ? doctoedit.lastname : '')
-	    + '"/></td></tr>'
-
-	+ '<tr><td>Address</td>'
-	+ '<td><textarea name="address" id="address" rows="4" cols="40">'
-	    + (doctoedit ? doctoedit.address : '')
-	    + '</textarea></td></tr>'
-	
-	+ '<tr><td>Phone number</td>'
-	+ '<td><input name="phonenumber" type="text" id="phonenumber" value="'
-	    + (doctoedit ? doctoedit.phonenumber : '')
-	    + '"/></td></tr>'
-
-
-	+ '<tr><td>Email</td>'
-	+ '<td><input name="email" type="text" id="email" value="'
-	    + (doctoedit ? doctoedit.email : '')
-	    + '"/></td></tr>'
-
-	+ '<tr><td>Notes</td>'
-	+ '<td><textarea name="notes" id="notes" rows="4" cols="40">'
-	    + (doctoedit ? doctoedit.notes : '')
-	    + '</textarea></td></tr>'
-	+ '</table><input type="submit" name="submit" class="update" value="'
-	+ (doctoedit ? 'Update' : 'Add') + '"/>'
-	+ '<input type="submit" name="submit" class="cancel" value="Cancel"/></form>';
-
-    var dialog = popup_dialog(formhtml);
-    var form = dialog.children("form#updatecustomer");
-    form.children("input.update").click( function(event) {
-    db.saveDoc(build_customer_doc_from_form(doctoedit, form),
-        { success: function() {
-            popup_cleanup(dialog);
-            initial_customer_list();
-        }});
-        return false;
-    });
-
-    form.children('input.cancel').click( function(event) {
-        popup_cleanup(dialog);
-        return false;
-    });
-
-    form.find("input#firstname").focus();
+    var form = new EditableForm({
+                    title: (doctoedit ? 'Edit' : 'Add') + ' Customer',
+                    modal: 1,
+                    fields: fields,
+                    buttons: [ { id: 'update', label: (doctoedit ? 'Update' : 'Add'), action: 'submit' },
+                               { id: 'cancel', label: 'Cancel' , action: 'remove' },
+                            ],
+                    submit: function(event) {
+                                var theform = this;
+                                db.saveDoc(build_customer_doc_from_form(doctoedit, theform),
+                                    { success: function() {
+                                        theform.remove();
+                                        initial_customer_list();
+                                    }}
+                                );
+                                return false;
+                            }
+                });
 }
 
 function build_customer_doc_from_form(doc,form) {
     if (!doc) {
-	doc = new Object;
+        doc = new Object;
     }
     var fields = ['firstname','lastname','address','phonenumber','email','notes'];
     for (var i in fields) {
         var fieldname = fields[i];
-        var formvalue = form.find("#" + fieldname).val();
+        var formvalue = form.valueFor(fieldname);
         if (formvalue == undefined) {
             formvalue = '';
         }
